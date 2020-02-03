95976abe4e29c2081754fc8a5cdf9b2dc38137ea
shagbag913 <sh4gbag913@gmail.com>
2020-02-03 16:14:28 -0500

    Add switch to wake to lockscreen or ambient display on wake gestures
    Cleaner version of
    https://github.com/ezio84/abc_packages_apps_Settings/commit/9502afd753ee0bcc7e9a98138f5bcfe835f043f1
    
    Change-Id: I0d27986c4bdde84238710898f35e863d7205510a

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -26,6 +26,7 @@ import androidx.annotation.VisibleForTesting;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
+import com.android.settings.display.AmbientWakeGesturesPreferenceController;
 import com.android.settings.display.PulseOnNewTracksPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
@@ -94,6 +95,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
         use(PulseOnNewTracksPreferenceController.class).setConfig(getConfig(context));
+        use(AmbientWakeGesturesPreferenceController.class).setConfig(getConfig(context));
     }
 
     @Override
